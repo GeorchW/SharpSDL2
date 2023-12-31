f023e0637ee09527905d834f127219357d349565
Adonis Deliannis <Blizzardo1@Blizzeta.net>
2023-12-31 00:11:42 -0800

    Added new Methods as well as documentation

--- src/SDL2_mixer.cs
+++ src/SDL2_mixer.cs
@@ -137,7 +137,7 @@ namespace SDL2
 			version.Patch = PatchLevel;
 		}
 
-		[DllImport(nativeLibName, EntryPoint = "MIX_Linked_Version", CallingConvention = CallingConvention.Cdecl)]
+		[DllImport(nativeLibName, EntryPoint = "Mix_Linked_Version", CallingConvention = CallingConvention.Cdecl)]
 		private static extern IntPtr INTERNAL_MIX_Linked_Version();
 		public static Version MIX_Linked_Version()
 		{
